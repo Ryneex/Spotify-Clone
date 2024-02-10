f717a7b86be2f1c1df206a30a707cf9e5abe68b8
MI Sajid <misajidsa@gmail.com>
2024-02-10 07:50:45 +0600

    Update base url

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,7 @@ const store = configureStore({
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <Provider store={store}>
-        <BrowserRouter>
+        <BrowserRouter basename="/Spotify-clone">
             <App />
         </BrowserRouter>
     </Provider>
